d4830c46825e2f3f0e382b6946a61cb2cf487044
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-06-27 22:29:52 -0400

    Update aws_lambda_permission.eventstolambda.tf

--- aws_lambda_permission.eventstolambda.tf
+++ aws_lambda_permission.eventstolambda.tf
@@ -1,8 +1,7 @@
 resource "aws_lambda_permission" "EventsToLambda" {
-  count         = "${length(var.rules)}"
   statement_id  = "EventsToLambda${count.index}"
   action        = "lambda:InvokeFunction"
   function_name = "${aws_lambda_function.awstoslack.function_name}"
   principal     = "events.amazonaws.com"
-  source_arn    = "${element(var.rules, count.index).arn}"
+  source_arn    = "${vars.event_rule_arn}"
 }
