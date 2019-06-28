df288e6aec34f273b2e04e1892b2d3ad5b4d2165
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-06-27 22:32:14 -0400

    Update aws_cloudwatch_event_target.snstoslack.tf

--- aws_cloudwatch_event_target.snstoslack.tf
+++ aws_cloudwatch_event_target.snstoslack.tf
@@ -1,5 +1,4 @@
 resource "aws_cloudwatch_event_target" "snstoslack" {
-  count = "${length(var.rules)}"
-  rule  = "${element(var.rules, count.index)}"
+  rule  = "${var.event_rule_arn}"
   arn   = "${aws_lambda_function.awstoslack.arn}"
 }
