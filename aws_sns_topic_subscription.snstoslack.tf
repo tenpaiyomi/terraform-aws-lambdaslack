dce9f772843433e7642c1ba6a87a9e26d57ce89c
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-06-27 22:37:06 -0400

    Update aws_sns_topic_subscription.snstoslack.tf

--- aws_sns_topic_subscription.snstoslack.tf
+++ aws_sns_topic_subscription.snstoslack.tf
@@ -1,5 +1,5 @@
 resource "aws_sns_topic_subscription" "snstoslack" {
-  topic_arn = "${vars.notification_topic_arn}"
+  topic_arn = "${var.notification_topic_arn}"
   protocol  = "LAMBDA"
   endpoint  = "${aws_lambda_function.awstoslack.arn}"
 }
