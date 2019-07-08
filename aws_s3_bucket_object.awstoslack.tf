aa8647fb015ae85cf31febb30da091fb7d0e45bd
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-07-08 11:07:01 -0400

    Use source and not content for the s3 file

--- aws_s3_bucket_object.awstoslack.tf
+++ aws_s3_bucket_object.awstoslack.tf
@@ -2,7 +2,7 @@ resource "aws_s3_bucket_object" "lambda-zip" {
   acl     = "public-read"
   bucket  = "${var.s3_bucket_name}"
   key     = "${var.s3_file_key}"
-  content = "${path.module}/lambda.zip"
+  source  = "${path.module}/lambda.zip"
 
   depends_on = [
     "data.archive_file.lambda",
