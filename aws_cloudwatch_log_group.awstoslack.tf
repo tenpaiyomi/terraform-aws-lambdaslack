82cf75511668e5b06a830f835e4d8ae36e9aa4b8
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-07-08 11:34:03 -0400

    Just manually set the name since we manually set the function name anyways

--- aws_cloudwatch_log_group.awstoslack.tf
+++ aws_cloudwatch_log_group.awstoslack.tf
@@ -1,5 +1,5 @@
 resource "aws_cloudwatch_log_group" "awstoslack" {
-  name                = "/aws/lambda/${aws_lambda_function.awstoslack.name}"
+  name                = "/aws/lambda/AWSToSlackHandler"
   retention_in_days   = 7
 
   depends_on = [
