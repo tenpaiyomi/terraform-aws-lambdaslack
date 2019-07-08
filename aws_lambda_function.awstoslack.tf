205232dc43ea2e766709b3633a5927e938a733ac
Eric Musgrove <tenpaiyomi@users.noreply.github.com>
2019-07-08 11:03:13 -0400

    Dropping filename

--- aws_lambda_function.awstoslack.tf
+++ aws_lambda_function.awstoslack.tf
@@ -5,7 +5,6 @@ resource "aws_lambda_function" "awstoslack" {
   role             = "${aws_iam_role.awstoslack.arn}"
   runtime          = "nodejs8.10"
   timeout          = "10"
-  filename         = "${path.module}/lambda.zip"
   s3_bucket        = "${var.s3_bucket_name}"
   s3_key           = "${var.s3_file_key}"
 
